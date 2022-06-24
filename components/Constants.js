d1bea559ee4cd9f4b775ad031b365cdff8381c51
Nathaniel Poulson <jnp2000@byu.edu>
2022-06-23 18:29:58 -0600

    add constants

---
+++ components/Constants.js
@@ -0,0 +1,8 @@
+// Constants.js
+import { Dimensions } from "react-native";
+export default {
+  MAX_WIDTH: Dimensions.get("screen").width,
+  MAX_HEIGHT: Dimensions.get("screen").height,
+  AVATAR_RADIUS: 50,
+  BLOCK_RADIUS: 50,
+};
