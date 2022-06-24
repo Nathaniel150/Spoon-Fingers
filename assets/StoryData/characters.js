6a2f772bbc0e3ffbcf86b74293f51888d221eac6
Amy Techavimol <amytechavimol@gmail.com>
2022-06-23 18:34:53 -0600

    Game data for story mode

---
+++ assets/StoryData/characters.js
@@ -0,0 +1,5 @@
+export let characters = new Map([
+    ["Sah", {name:"Sah", photo:require("../CharacterProfileImages/sah.png"), backgroundColor: "yellow"}],
+    ["prisoner1", {name:"Nathaniel", photo:require("../CharacterProfileImages/prisoner1.png"), backgroundColor: "pink"}],
+    
+]);
---
+++ assets/StoryData/scenes.js
@@ -0,0 +1,84 @@
+import { characters } from "./characters";
+
+export let storyP1= [
+    {
+        type: "narration",
+        text: "The prison walls quivered as the new prisoner approached",
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "narration",
+        text: "They sensed something in the air...",
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "narration",
+        text: "A dangerous foe would soon call the prison home. But not for long",
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "narration",
+        text: "This is the legend of SPOONFINGERS",
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "Maybe I shouldn't have done that",
+        character: characters.get("Sah"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "I can't believe I got caught",
+        character: characters.get("Sah"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "I mean it wasn't even my gun...",
+        character: characters.get("Sah"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "...or my children",
+        character: characters.get("Sah"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "There is no way we can stay in here with these losers",
+        character: characters.get("Sah"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "Come on boys, let’s get outta here",
+        character: characters.get("Sah"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "yeah yeah",
+        character: characters.get("prisoner1"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+    {
+        type: "dialogue",
+        text: "I bet I can pick this lock. This looks just like a game I know!",
+        character: characters.get("Sah"),
+        bkgdImage: require("../BackgroundImages/background.jpeg")
+
+    },
+];
