d2e977c0a52e352f41e9470620ec2fa84dc50428
Amy Techavimol <amytechavimol@gmail.com>
2022-07-18 16:34:09 -0600

    Functionality of lockle complete

--- components/GameComponents/Wordle/Lockle.js
+++ components/GameComponents/Wordle/Lockle.js
@@ -2,19 +2,16 @@ import React from 'react';
 import Wordle from './Wordle';
 import { StyleSheet, View, Button, Text  } from 'react-native';
 import { Dialog, DialogHeader, DialogContent, DialogActions, Provider} from "@react-native-material/core";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { colors } from "./wordleConstants";
 import { set } from 'react-native-reanimated';
+import uuid from "react-native-uuid"
 
 //Game 1(3minutes): Pop up(introducing the game and how to play), gameplay, pop-up if won or lost and trasinition to next game,
 //Game 2(2minutes): pop up, gameplay, pop-up if won or lost and transition to last game
 //Game 3 (1minute):pop up (last one, the guards are approaching), gameplay, pop up,
 //Test comment
-function Lockle(props) {
-    const [won, setWon] = useState(false);
-    const [visible, setVisible] = useState(false);
-    const [startScreenVisible, setStartScreenVisible] = useState(true);
-    const [level, setLevel] = useState(0)
+function Lockle({updateState}) {
 
     const words = ["spoon", "knife", "guard", "loser", "fight", "under", "brick", "pixel", "mouse"]
 
@@ -23,42 +20,72 @@ function Lockle(props) {
             startText: "Level 1",
             word: "spoon",
             endText: "endText 1",
-            time: 60
+            time: 180,
+            key: uuid.v4()
         },
         {
             startText: "Level 2",
             word: "knife",
             endText: "endText 2",
-            time: 120
+            time: 120,
+            key: uuid.v4()
         },
         {
             startText: "Level 3",
             word: "forks",
             endText: "endText 3",
-            time: 180
+            time: 60,
+            key: uuid.v4()
         }
 
     ]
 
+    const [won, setWon] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const [startScreenVisible, setStartScreenVisible] = useState(true);
+    const [level, setLevel] = useState(0)
+    const [timerOn, setTimerOn] = useState(false)
+
     const checkandUpdateGame = () => {
         if(won) {
-            setLevel(level+1)
+            if(level < wordleContent.length - 1) {
+                setLevel(level + 1)
+            } else {
+                updateState()
+            }
+
         } else {
             setLevel(0)
         }
-        setVisible(false);
-        setStartScreenVisible(true);
+
+       setVisible(false);
+       setStartScreenVisible(true);
     
     }
+
+    const startGame = () => {
+        setStartScreenVisible(false)
+        setTimerOn(true)
+    }
+
+    
 
     return (
         
         
         <View style={styles.container}>
-            <Wordle setVisible={setVisible} setWon={setWon} targetWord={wordleContent[level].word}/>
+            {console.log(wordleContent[level].key)}
+            <Wordle 
+                setVisible={setVisible} 
+                setWon={setWon} 
+                targetWord={wordleContent[level].word} 
+                timerOn={timerOn} 
+                setTimerOn={setTimerOn}
+                timeLimit={wordleContent[level].time}
+                key={wordleContent[level].key}/>
+
             <Provider >
 
-            {/* end popup */}
             <Dialog visible={visible}  >
                 <DialogHeader title={won === true ? "WON" : "LOST"} />
                 <DialogContent style={ styles.dialog}>
@@ -86,7 +113,7 @@ function Lockle(props) {
                     title="Ok"
                     compact
                     variant="text"
-                    onPress={() => setStartScreenVisible(false)}
+                    onPress={() => startGame()}
                 />
                 </DialogActions>
             </Dialog>
@@ -103,6 +130,9 @@ function Lockle(props) {
      },
      dialog: {
          backgroundColor: colors.green,
+     },
+     button: {
+
      }
  });
 export default Lockle;
