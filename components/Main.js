baf4d8bbecc1f44fed606072ad6dcd53d2dbf316
Amy Techavimol <amytechavimol@gmail.com>
2022-07-19 21:13:16 -0600

    Imports story p4

--- components/Main.js
+++ components/Main.js
@@ -4,10 +4,10 @@ import { useState } from "react";
 import DialogueScene from "./SceneComponents/DialogueScene";
 import StoryScene from "./SceneComponents/StoryScene";
 import Wordle from "./GameComponents/Wordle/Wordle";
-import Battleship from "./GameComponents/Battleship/Battleship";
+import BattleShip from "./GameComponents/Battleship/Battleship";
 import Digdug from "./GameComponents/Digdug/Digdug";
 
-import { storyP1, storyP2, storyP3 } from "../assets/StoryData/scenes";
+import { storyP1, storyP2, storyP3, storyP4 } from "../assets/StoryData/scenes";
 import Lockle from "./GameComponents/Wordle/Lockle";
 
 const Main = () => {
@@ -19,7 +19,7 @@ const Main = () => {
   };
 
   if (stateTracker == 1) {
-    return <StoryScene updateState={updateState} story={storyP1} />;
+    return <StoryScene updateState={updateState} story={storyP1}/>;
   } else if (stateTracker == 2) {
     return <Lockle updateState={updateState} />;
   } else if (stateTracker == 3 ) {
@@ -39,3 +39,6 @@ const Main = () => {
 };
 
 export default Main;
+
+
+
