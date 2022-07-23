5719d8ce5f00f1a7079c157e60db72f71dfced5c
Nathaniel Poulson <jnp2000@byu.edu>
2022-07-23 13:47:36 -0600

    reset changed state in Main.js

--- components/Main.js
+++ components/Main.js
@@ -12,7 +12,7 @@ import Lockle from "./GameComponents/Wordle/Lockle";
 
 const Main = () => {
   //use to track what part ofe game we are currently in;
-  const [stateTracker, setStateTracker] = useState(4);
+  const [stateTracker, setStateTracker] = useState(1);
 
   const updateState = () => {
     setStateTracker(stateTracker + 1);
