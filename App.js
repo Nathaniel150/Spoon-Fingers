8c512b6e6003a62359959eaac6f51361b7f00448
Nathaniel Poulson <jnp2000@byu.edu>
2022-08-08 17:08:16 -0600

    Auto-formatted App.js

--- App.js
+++ App.js
@@ -1,33 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+// import Main from "./components/Main";
 import Main2 from "./components/Main2.0";
 
-import { useState } from 'react';
+import { useState } from "react";
 import * as Font from "expo-font";
-import AppLoading from 'expo-app-loading';
-
+import AppLoading from "expo-app-loading";
 
 export default function App() {
   const [fontsloaded, setFontsLoaded] = useState(false);
 
   const getFonts = () =>
-        Font.loadAsync({
-            PixeloidSansBold: require("./assets/fonts/PixeloidSans-Bold.ttf"),
-            PixeloidSans: require("./assets/fonts/PixeloidSans.ttf"),
-        });    
+    Font.loadAsync({
+      PixeloidSansBold: require("./assets/fonts/PixeloidSans-Bold.ttf"),
+      PixeloidSans: require("./assets/fonts/PixeloidSans.ttf"),
+    });
 
   if (fontsloaded) {
-    return <Main2/>
+    return <Main2 />;
   } else {
-      return (
-        <AppLoading
+    return (
+      <AppLoading
         startAsync={getFonts}
         onFinish={() => {
-            setFontsLoaded(true);
+          setFontsLoaded(true);
         }}
         onError={console.warn}
-        />
-      );
+      />
+    );
   }
 }
 
@@ -42,11 +42,10 @@ const styles = StyleSheet.create({
 
 const fontStyles = StyleSheet.create({
   pixelBoldFont: {
-    fontFamily: 'PixeloidSansBold'
+    fontFamily: "PixeloidSansBold",
   },
   pixelFont: {
-    fontFamily: 'PixeloidSans'
-  }
-
-})
-export {fontStyles}
+    fontFamily: "PixeloidSans",
+  },
+});
+export { fontStyles };
