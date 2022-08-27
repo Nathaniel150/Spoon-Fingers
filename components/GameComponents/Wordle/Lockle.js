bbed8bed34d1c6c4aa073e8ac4818e5e0e4de1b4
Amy Techavimol <amytechavimol@gmail.com>
2022-08-26 18:32:02 -0600

    Pop up clean up

--- components/GameComponents/Wordle/Lockle.js
+++ components/GameComponents/Wordle/Lockle.js
@@ -36,18 +36,21 @@ function Lockle({ navigation, route }) {
       word: "spoon",
       endText: "endText 1",
       time: 180,
+      winText: "One down! You're starting to get the hang of this."
     },
     {
       startText: "Level 2",
       word: "knife",
       endText: "endText 2",
       time: 120,
+      winText: "Nice! So close to getting out of this cell! One last passcode."
     },
     {
       startText: "Level 3",
       word: "forks",
       endText: "endText 3",
       time: 60,
+      winText: "Hurray! You cracked all three locks"
     },
   ];
 
@@ -92,8 +95,9 @@ function Lockle({ navigation, route }) {
 
       <Popup 
         visible={visible}
-        text={won ? "You won! Onto the next level." : "You lost, guess you aren't breaking out today."}
+        text={won ? wordleContent[level].winText : "You lost, guess you aren't breaking out today."}
         button1={{ title: "Next", onPress: () => checkandUpdateGame() }}
+
         />
 
       {/* <Provider>
